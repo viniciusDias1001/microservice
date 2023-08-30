6c15a43c02084d013ddc8aacd5a137fa2129022f
Vinicius Dias <105192228+viniciusDias1001@users.noreply.github.com>
2023-08-30 17:16:56 -0300

    Eureka-Service

---
+++ Eureka-Service/src/test/java/io/github/ViniciusDias1001/NameServe/NameServeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ViniciusDias1001.NameServe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
